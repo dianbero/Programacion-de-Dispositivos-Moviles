2c2afa75c69effbbf3b4eb4eb6c64a4f774f5c6c
Diana <dianbero@gmail.com>
2019-10-18 12:48:55 +0200

    little step 02

--- TryReproductor/app/src/main/java/es/iesnervion/diana/tryreproductor/Adapters/AdapterCanciones.java
+++ TryReproductor/app/src/main/java/es/iesnervion/diana/tryreproductor/Adapters/AdapterCanciones.java
@@ -1,17 +1,20 @@
 package es.iesnervion.diana.tryreproductor.Adapters;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import java.util.ArrayList;
 
 import es.iesnervion.diana.tryreproductor.Clases.Canciones;
+import es.iesnervion.diana.tryreproductor.R;
 
 public class AdapterCanciones extends BaseAdapter {
 
     private Activity actividad;
     private ArrayList<Canciones> arrayListCanciones;
+
     public AdapterCanciones(Activity actividad, ArrayList<Canciones> arrayListCanciones){
         this.actividad = actividad;
         this.arrayListCanciones = arrayListCanciones;
@@ -19,21 +22,29 @@ public class AdapterCanciones extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return arrayListCanciones.size();//Devuelve todas la canciones del arrayList
+        //Devuelve la cancitdad de elementos(canciones) del arrayList, que son la cantidad de elementos que contiene el adapter
+        return arrayListCanciones.size();
     }
 
     @Override
-    public Object getItem(int posicion) {//Devuelve la lista de elementos del arrayList en la posicion especificada
+    public Object getItem(int posicion) {//Devuelve la lista de elementos del arrayList en la posicion especificada por parametro
         return arrayListCanciones.get(posicion);
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int posicion) {//Devuelve el elemento seleccionado del arrayList
+
+        return posicion;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View view, ViewGroup viewGroup)// muestra los elementos dentro del ListView
+    {
+        //Crea inflater para cada layout de cada fila de lista (cada ListView)
+        if(view == null){
+            //resource: Se le pasa id del layout a cargar(inflar), root: view a la que se acopla el recuso como hijo si el attachRoot es True, attachToRoot: ser o no ser padre del recurso infladdo
+            view = LayoutInflater.from(actividad).inflate(R.layout.activity_list_view_canciones, viewGroup, false);
+        }
+        return view;
     }
 }
