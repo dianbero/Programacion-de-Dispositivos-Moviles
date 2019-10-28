90fb859743adffc3695f35d711ea1a7d79b4fde3
dianbero <dianbero@gmail.com>
2019-10-28 02:17:38 +0100

    Cámara casi hecha

--- app/src/main/java/es/iesnervion/diana/appabrircamaramovil/MainActivity.java
+++ app/src/main/java/es/iesnervion/diana/appabrircamaramovil/MainActivity.java
@@ -3,6 +3,9 @@ package es.iesnervion.diana.appabrircamaramovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -11,14 +14,17 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    ImageView image;
-    Button btnCamara;
+    ImageView image, image2;
+    Button btnCamara, btnVolver;
+    private static final int CAMERA_PICTURE_ID = 1;
 
     /*
     * Info para App Cámara:
     * https://stackoverflow.com/questions/5991319/capture-image-from-camera-and-display-in-activity
     * De Página Android:
-    * https://developer.android.com/training/camera/photobasics*/
+    * https://developer.android.com/training/camera/photobasics
+    * GeeksForGeeks:
+    * https://www.geeksforgeeks.org/android-how-to-open-camera-through-intent-and-display-captured-image/*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,7 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCamara = (Button) findViewById(R.id.btnCamara);
+        //image = findViewById(R.id.imagenCamara);
+
+        btnCamara = findViewById(R.id.btnCamara);
+        btnCamara.setOnClickListener(this);
+        //setContentView(btnCamara);
 
 
     }
@@ -35,15 +45,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         //Intent intentOpenCamera = new Intent("android.media.action.IMAGE_CAPTURE"); //si falla probar, sin comillas: android.provider.MediaStore.ACTION_IMAGE_CAPTURE
-        Intent intentOpenCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        //startActivityForResult(intentOpenCamera, Integer.parseInt(CAMERA_SERVICE));
-        startActivityForResult(intentOpenCamera, );
-        //startActivity(intentOpenCamera);
+        Intent intentOpenCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intentOpenCamera, CAMERA_PICTURE_ID);
     }
 
     protected void  onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode == CAMERA_PIC_REQUEST){
+        setContentView(R.layout.layout_imagen_camara);
+        image2 = findViewById(R.id.imagenCamara2);
+        btnVolver = findViewById(R.id.btnVolver);
+        //Vuelve a la página de foto
+        btnVolver.setOnClickListener(this);
+
+        if(requestCode == 1){
+            //Convierte la imagen a un formato BitMap para almacenar
+            Bitmap imagen = (Bitmap) data.getExtras().get("data");
+            //Manda la imagen obtenida en la vista image2
+            image2.setImageBitmap(imagen);
 
         }
     }
+
+
 }
+
+
+
