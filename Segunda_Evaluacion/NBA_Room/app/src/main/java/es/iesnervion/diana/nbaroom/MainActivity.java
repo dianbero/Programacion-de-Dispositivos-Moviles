4088e8a3a00f58c3afba83b853308abda64b26dc
Diana <dianbero@gmail.com>
2020-01-17 11:11:32 +0100

    pf.. beh

--- Segunda_Evaluacion/NBA_Room/app/src/main/java/es/iesnervion/diana/nbaroom/MainActivity.java
+++ Segunda_Evaluacion/NBA_Room/app/src/main/java/es/iesnervion/diana/nbaroom/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Help:
+        * cómo guardar contenido en BBDD con Room:
+        * https://developer.android.com/training/data-storage/room
+        * Steps to implement Room library:
+        * https://android.jlelse.eu/5-steps-to-implement-room-persistence-library-in-android-47b10cd47b24
+        * */
     }
 }
