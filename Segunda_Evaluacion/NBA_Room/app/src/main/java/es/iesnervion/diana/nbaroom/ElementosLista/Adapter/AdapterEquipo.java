e1154d3e5142c668f13406215b1c630032e78fda
Diana <dianbero@gmail.com>
2020-01-23 11:11:37 +0100

    ...

--- Segunda_Evaluacion/NBA_Room/app/src/main/java/es/iesnervion/diana/nbaroom/ElementosLista/Adapter/AdapterEquipo.java
+++ Segunda_Evaluacion/NBA_Room/app/src/main/java/es/iesnervion/diana/nbaroom/ElementosLista/Adapter/AdapterEquipo.java
@@ -1,7 +1,39 @@
 package es.iesnervion.diana.nbaroom.ElementosLista.Adapter;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class AdapterEquipo {
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import es.iesnervion.diana.nbaroom.Entities.Equipo;
+
+public class AdapterEquipo extends ArrayAdapter<Equipo> {
+
+    public AdapterEquipo(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    public AdapterEquipo(@NonNull Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+    }
+
+    public AdapterEquipo(@NonNull Context context, int resource, @NonNull Equipo[] objects) {
+        super(context, resource, objects);
+    }
+
+    public AdapterEquipo(@NonNull Context context, int resource, int textViewResourceId, @NonNull Equipo[] objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+
+    public AdapterEquipo(@NonNull Context context, int resource, @NonNull List<Equipo> objects) {
+        super(context, resource, objects);
+    }
+
+    public AdapterEquipo(@NonNull Context context, int resource, int textViewResourceId, @NonNull List<Equipo> objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
 }
