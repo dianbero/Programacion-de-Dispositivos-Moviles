f3d5c04be9589128aafb77852d91a7619b4ac192
Diana <dianbero@gmail.com>
2020-02-17 10:15:43 +0100

    al guardar nuevos datos falla algo en método guardarDatosEnLista

--- Segunda_Evaluacion/PracticaExamen2Eval/PracticaListYRoom/app/src/main/java/es/iesnervion/diana/practicalistyroom/MainActivity.java
+++ Segunda_Evaluacion/PracticaExamen2Eval/PracticaListYRoom/app/src/main/java/es/iesnervion/diana/practicalistyroom/MainActivity.java
@@ -6,40 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 import es.iesnervion.diana.practicalistyroom.Activities.ListaUsuariosActivity;
 import es.iesnervion.diana.practicalistyroom.Activities.PruebaFuncionamiento;
+import es.iesnervion.diana.practicalistyroom.Models.Usuario;
 import es.iesnervion.diana.practicalistyroom.Tools.ListaUsuarios;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtNombre;
+    EditText txtEdad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button button = (Button) findViewById(R.id.btnLogin);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), ListaUsuariosActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        //TODO guardar datos insertados del login en la lista
+        //TODO añadir otro botón al inicio para mostrar directamente la lista sin añadir nuevo usuario
+
+        txtNombre = (EditText) findViewById(R.id.loginNombre);
+        txtEdad = (EditText) findViewById(R.id.loginEdad);
+
+
     }
 
-
-//    public void login(View view) {
-//        if (view.getId() == R.id.btnLogin) {
-//            Intent intent = new Intent(getApplicationContext(), ListaUsuariosActivity.class);
-//            startActivity(intent);
-//        }
-//    }
-
     public void login(View view) {
+
+//        guardarDatosEnLista();
+
         if (view.getId() == R.id.btnLogin) {
             Intent intent = new Intent(this, ListaUsuariosActivity.class);
             startActivity(intent);
         }
+    }
+
+    public ArrayList<Usuario> guardarDatosEnLista() {
+        String nombre = txtNombre.getText().toString();
+        int edad = Integer.parseInt(txtEdad.getText().toString());
+
+        return new ListaUsuarios().addNuevoUsuario(nombre, edad);
     }
 }
