cc13014931ae58ebf109608f77fe9db2020783fb
Diana <dianbero@gmail.com>
2020-02-20 12:52:49 +0100

    TODO: obligar a introducir nombre

--- Segunda_Evaluacion/PracticaExamen2Eval/PracticaListYRoom/app/src/main/java/es/iesnervion/diana/practicalistyroom/Activities/UsuarioRegistradoActivity.java
+++ Segunda_Evaluacion/PracticaExamen2Eval/PracticaListYRoom/app/src/main/java/es/iesnervion/diana/practicalistyroom/Activities/UsuarioRegistradoActivity.java
@@ -3,18 +3,41 @@ package es.iesnervion.diana.practicalistyroom.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import es.iesnervion.diana.practicalistyroom.R;
 
 public class UsuarioRegistradoActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.usuario_registrado);
 
         /*Help:
-        * https://stackoverflow.com/questions/4186021/how-to-start-new-activity-on-button-click
-        * */
+         * https://stackoverflow.com/questions/4186021/how-to-start-new-activity-on-button-click
+         * */
 
+        TextView txtNombre = (TextView) findViewById(R.id.txtNombre);
+        TextView txtEdad = (TextView) findViewById(R.id.txtEdad);
+
+        //Obtengo el intent
         Intent intent = getIntent();
+
+        //Comprueba que tenga datos
+        if (intent.getExtras() != null) {
+            String nombre = intent.getStringExtra("nombre");
+            int edad = intent.getIntExtra("edad", 22);
+
+            //Paso los datos obtenidos del intent a los elementos de la vista
+            txtNombre.setText(nombre);
+            txtEdad.setText(Integer.toString(edad));
+        }
+
+
     }
+
+
+    //Mostrar nombre y edad
 }
--- Segunda_Evaluacion/PracticaExamen2Eval/PracticaListYRoom/app/src/main/java/es/iesnervion/diana/practicalistyroom/MainActivity.java
+++ Segunda_Evaluacion/PracticaExamen2Eval/PracticaListYRoom/app/src/main/java/es/iesnervion/diana/practicalistyroom/MainActivity.java
@@ -3,6 +3,7 @@ package es.iesnervion.diana.practicalistyroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 
 import es.iesnervion.diana.practicalistyroom.Activities.ListaUsuariosActivity;
 import es.iesnervion.diana.practicalistyroom.Activities.PruebaFuncionamiento;
+import es.iesnervion.diana.practicalistyroom.Activities.UsuarioRegistradoActivity;
 import es.iesnervion.diana.practicalistyroom.Models.Usuario;
 import es.iesnervion.diana.practicalistyroom.Tools.ListaUsuarios;
 
@@ -33,20 +35,48 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+//    public void login(View view) {
+//
+////        ArrayList<Usuario> lista =  guardarDatosEnLista();
+//
+//    }
+
+    //Probando:
     public void login(View view) {
+        Intent intent = new Intent(this, UsuarioRegistradoActivity.class);
+        boolean isVacio;
 
-//        ArrayList<Usuario> lista =  guardarDatosEnLista();
-
-    }
-
-    public ArrayList<Usuario> guardarDatosEnLista() {
+        //Obtengo los datos de la vista
         String nombre = txtNombre.getText().toString();
-        int edad = Integer.parseInt(txtEdad.getText().toString());
+        //Si la entrada de edad no está vacía lo asigna a un entero
+        int edad = 0;
+        if (!txtEdad.getText().toString().equals("")) {
+            edad = Integer.parseInt(txtEdad.getText().toString());
+            intent.putExtra("nombre", nombre);
+            intent.putExtra("edad", edad);
+            startActivity(intent);
 
-        return new ListaUsuarios().addNuevoUsuario(nombre, edad);
+        } else {
+            txtEdad.setText("Must add an age");
+            txtEdad.setTextColor(Color.RED);
+        }
+
+
+        //Identifico los datos para pasar al intent
+
+
     }
 
+
+    //    public ArrayList<Usuario> guardarDatosEnLista() {
+//        String nombre = txtNombre.getText().toString();
+//        int edad = Integer.parseInt(txtEdad.getText().toString());
+//
+//        return new ListaUsuarios().addNuevoUsuario(nombre, edad);
+//    }
+//
     public void verLista(View view) {
+        //Si el id del elemento de la vista se corresponde con el del botón para ir a la vista de lista
         if (view.getId() == R.id.btnLista) {
             Intent intent = new Intent(this, ListaUsuariosActivity.class);
 //            intent.putExtra("lista", lista);
